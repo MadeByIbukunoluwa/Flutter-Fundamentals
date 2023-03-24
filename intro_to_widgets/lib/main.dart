6d0089255bf99f23a593f282f926c0adb2907d53
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-24 21:07:32 +0100

    feat:added basic material component layout

--- intro_to_widgets/lib/main.dart
+++ intro_to_widgets/lib/main.dart
@@ -73,10 +73,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    title: 'My app',
-    home: SafeArea(
-      child: MyScaffold(),
-    ),
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
   ));
 }
+
+class TutorialHome extends StatelessWidget {
+  const TutorialHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: const IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation Menu',
+            onPressed: null,
+          ),
+          title: const Text('Example title'),
+          actions: const [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: null,
+            )
+          ]),
+      body: const Center(
+        child: Text('hello world'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add',
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+//Handling Gestures 
 
