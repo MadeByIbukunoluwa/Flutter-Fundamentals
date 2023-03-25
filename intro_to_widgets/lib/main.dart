5a6be6befc83322a3733373dca7c4eff4aecaac2
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-25 02:24:10 +0100

    feat:added a complex example

--- intro_to_widgets/lib/main.dart
+++ intro_to_widgets/lib/main.dart
@@ -71,42 +71,184 @@ import 'package:flutter/material.dart';
 
 //Using Material Components
 
-void main() {
-  runApp(const MaterialApp(
-    title: 'Flutter Tutorial',
-    home: TutorialHome(),
-  ));
-}
+// void main() {
+//   runApp(const MaterialApp(
+//     title: 'Flutter Tutorial',
+//     home: TutorialHome(),
+//   ));
+// }
 
-class TutorialHome extends StatelessWidget {
-  const TutorialHome({super.key});
+// class TutorialHome extends StatelessWidget {
+//   const TutorialHome({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          leading: const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation Menu',
-            onPressed: null,
-          ),
-          title: const Text('Example title'),
-          actions: const [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: null,
-            )
-          ]),
-      body: const Center(
-        child: Text('hello world'),
-      ),
-      floatingActionButton: const FloatingActionButton(
-        tooltip: 'Add',
-        onPressed: null,
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-//Handling Gestures 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//           leading: const IconButton(
+//             icon: Icon(Icons.menu),
+//             tooltip: 'Navigation Menu',
+//             onPressed: null,
+//           ),
+//           title: const Text('Example title'),
+//           actions: const [
+//             IconButton(
+//               icon: Icon(Icons.search),
+//               onPressed: null,
+//             )
+//           ]),
+//       body: const Center(
+//         child: Text('hello world'),
+//       ),
+//       floatingActionButton: const FloatingActionButton(
+//         tooltip: 'Add',
+//         onPressed: null,
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+//Handling Gestures
+// Lets build a basic button which detects input gestures
+
+// class MyButton extends StatelessWidget {
+//   const MyButton({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: () {
+//         print('MyButton was Tapped');
+//       },
+//       child: Container(
+//           height: 50.0,
+//           padding: const EdgeInsets.all(8.0),
+//           margin: const EdgeInsets.symmetric(horizontal: 8.0),
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(5.0),
+//             color: Colors.lightGreen[500],
+//           ),
+//           child: const Center(
+//             child: Text('Engage'),
+//           )),
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(
+//     const MaterialApp(
+//       home:Scaffold(
+//         body:Center(
+//           child:MyButton(),
+//         )
+//       )
+//     )
+//     );
+// }
+
+//Changing widgets in reponse to input
+
+// class Counter extends StatefulWidget {
+//   const Counter({super.key});
+
+//   @override
+//   State<Counter> createState() => _CounterState();
+// }
+
+// class _CounterState extends State<Counter> {
+//   int _counter = 0;
+
+//   void _increment() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: <Widget>[
+//         ElevatedButton(
+//           onPressed: _increment,
+//           child: const Text('increment'),
+//         ),
+//         const SizedBox(width: 16),
+//         Text('Count: $_counter'),
+//       ],
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(const MaterialApp(
+//       home: Scaffold(
+//           body: Center(
+//     child: Counter(),
+//   ))));
+// }
+
+// more complex example
+// in this example, there is creation of new stateless widgets , cleanly separating theconcerns of displaying the counter and chnaging the counter
+
+// class CounterDisplay extends StatelessWidget {
+//   const CounterDisplay({required this.count, super.key});
+
+//   final int count;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text('Count: $count');
+//   }
+// }
+
+// class CounterIncrementor extends StatelessWidget {
+//   const CounterIncrementor({required this.onPressed, super.key});
+
+//   final VoidCallback onPressed;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton(onPressed: onPressed, child: const Text('Increment'));
+//   }
+// }
+
+// class Counter extends StatefulWidget {
+//   const Counter({super.key});
+
+//   @override
+//   State<Counter> createState() => _CounterState();
+// }
+
+// class _CounterState extends State<Counter> {
+//   int _counter = 0;
+
+//   void _increment() {
+//     setState(() {
+//       ++_counter;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: <Widget>[
+//         CounterIncrementor(onPressed: _increment),
+//         const SizedBox(width: 16),
+//         CounterDisplay(count: _counter),
+//       ],
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(const MaterialApp(
+//       home: Scaffold(
+//           body: Center(
+//     child: Counter(),
+//   ))));
+// }
+
 
