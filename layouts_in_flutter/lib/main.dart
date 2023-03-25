093b6f67aa9603fe91bbd070f8bc691b66940b20
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-25 17:47:54 +0100

    feat:added a _buildcard Widget

--- layouts_in_flutter/lib/main.dart
+++ layouts_in_flutter/lib/main.dart
@@ -20,7 +20,8 @@ class MyApp extends StatelessWidget {
             title: const Text('Flutter layout demo'),
           ),
           // body: Center(child: buildHomePage('Strawberry Pavlova Recipe'))),
-          body: Center(child: _buildList())),
+          // body: Center(child: _buildList())),
+          body: Center(child: _buildStack())),
     );
   }
 
@@ -239,4 +240,70 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
+  //Stack
+  Widget _buildStack() {
+    return Stack(
+      alignment: const Alignment(0.6, 0.6),
+      children: [
+        const CircleAvatar(
+          backgroundImage: AssetImage('images/pic0.jpg'),
+          radius: 100,
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            color: Colors.black45,
+          ),
+          child: const Text('Mia B',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              )),
+        )
+      ],
+    );
+  }
 }
+
+Widget _buildCard() {
+  return SizedBox(
+    height:200,
+    child:Card(
+        child:Column(
+            children: [
+              ListTile(
+                title:const Text(
+                    '1625 Main Street',
+                    style:TextStyle(fontWeight: FontWeight.w500)
+                    ),
+                subtitle: const Text('My City, CA 99984'),
+                leading:Icon(
+                  Icons.restaurant_menu,
+                  color:Colors.blue[500]
+                ),
+              ),
+              const Divider(),
+              ListTile(
+                title: const Text(
+                    '(408) 555-1212',
+                    style:TextStyle(fontWeight:FontWeight.w500)
+                ),
+                leading: Icon(
+                    Icons.contact_phone,
+                    color:Colors.blue[500]
+                )
+              ),
+              ListTile(
+                title:const Text('costa@example.com'),
+                leading:Icon(
+                  Icons.contact_mail,
+                  color:Colors.blue[500],
+                )
+              )
+            ],
+        )
+    ),
+  );
+}
+
