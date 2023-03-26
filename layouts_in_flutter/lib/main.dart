4c867c0f794676af9b5f71fa86f4b601f872d99e
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-26 08:03:33 +0100

    feat:added and modified file

--- layouts_in_flutter/lib/main.dart
+++ layouts_in_flutter/lib/main.dart
@@ -15,21 +15,40 @@ enum ListDemoType {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter layout Demo',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter layout demo'),
-          ),
-          // body: Center(child: buildHomePage('Strawberry Pavlova Recipe'))),
-          // body: Center(child: _buildList())),
-          body: Center(child: _buildStack())),
-          // body: Center(child: _buildListIntl())),
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
     );
+    return MaterialApp(
+        title: 'Flutter layout Demo',
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Flutter layout demo'),
+            ),
+            // body: Center(child: buildHomePage('Strawberry Pavlova Recipe'))),
+            // body: Center(child: _buildList())),
+            // body: Center(child: _buildStack())),
+            body: Column(
+              children: [
+                Image.asset('images/lake.jpg',
+                    width: 600, height: 240, fit: BoxFit.cover),
+                titleSection,
+                buttonSection,
+                textSection
+              ],
+            )
+            // body: Center(child: _buildListIntl())),
+
+            ));
   }
 
 // Row and Column
@@ -301,8 +320,7 @@ Widget _buildCard() {
   );
 }
 
-
-// This widget leads to a null check used on null value error 
+// This widget leads to a null check used on null value error
 Widget _buildListIntl() {
   return ListDemo(type: ListDemoType.twoLine);
 }
@@ -330,7 +348,8 @@ class ListDemo extends StatelessWidget {
                     leading: ExcludeSemantics(
                       child: CircleAvatar(child: Text('$index')),
                     ),
-                    title: Text(AppLocalizations.of(context)!.demoBottomSheetItem(index)),
+                    title: Text(AppLocalizations.of(context)!
+                        .demoBottomSheetItem(index)),
                     subtitle: type == ListDemoType.twoLine
                         ? Text(AppLocalizations.of(context)!.demoListTitle)
                         : null),
@@ -339,3 +358,64 @@ class ListDemo extends StatelessWidget {
         ));
   }
 }
+
+//Simple Layout practical example
+Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: const Text(
+                'Oeschinen Lale CampGround',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Text('Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                )),
+          ],
+        )),
+        Icon(Icons.star, color: Colors.red[500]),
+        const Text('41')
+      ],
+    ));
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(
+        icon,
+        color: color,
+      ),
+      Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              )))
+    ],
+  );
+}
+
+Widget textSection = const Padding(
+  padding: EdgeInsets.all(32),
+  child: Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
