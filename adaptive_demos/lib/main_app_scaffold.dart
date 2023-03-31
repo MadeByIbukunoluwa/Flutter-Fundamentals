7d35a6b53d246b42cd0fcf0244e8c17004ac44c3
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-03-31 10:26:38 +0100

    feat:modified and added code to main_app_scaffold.dart

--- adaptive_demos/lib/main_app_scaffold.dart
+++ adaptive_demos/lib/main_app_scaffold.dart
@@ -19,9 +19,112 @@ import 'global/targeted_actions.dart';
 List<Widget> getMainMenuChildren(BuildContext context) {
   // Define a method to change pages in the AppModel
 
-  void chnagePage(int value) => context.read<AppModel>().selectedIndex = value;
+  void changePage(int value) => context.read<AppModel>().selectedIndex = value;
   int index = context.select((AppModel m) => m.selectedIndex);
   return [
-    SelectedPageButton()
+    SelectedPageButton(
+      onPressed: () => changePage(0),
+      label: "Adaptive Grid",
+      isSelected: index == 0,
+    ),
+    SelectedPageButton(
+      onPressed: () => changePage(0),
+      label: "Adaptive Data Table",
+      isSelected: index == 1,
+    ),
+    SelectedPageButton(
+      onPressed: () => changePage(0),
+      label: "Adaptive Reflow",
+      isSelected: index == 2,
+    ),
+    SelectedPageButton(
+      onPressed: () => changePage(0),
+      label: "Focus Examples",
+      isSelected: index == 3,
+    ),
   ];
+}
+
+//Uses a tab navigation + drawer, or a side menu that combines both
+
+class MainAppScaffold extends StatefulWidget {
+  @override
+  _MainAppScaffoldState createState() => _MainAppScaffoldState();
+}
+
+class _MainAppScaffoldState extends State<MainAppScaffoldState> {
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  Widget build(BuildContext context) {
+    bool useTabs = MediaQuery.of(context).size.width < FormFactor.tablet;
+    bool isLoggedOut = context.select((AppModel m) => m.isLoggedIn) = false;
+    return TargetedActionScope(
+       
+    )
+  }
+}
+
+
+
+
+class _SideMenu extends StatelessWidget {
+  const _SideMenu({Key? key, this.showPageButtons = true}) : super(key: key);
+
+  final bool showPageButtons;
+
+  @override
+  Widget build(BuildContext context) {
+    void _handleLogoutPressed() async {
+      String message = "Are you sure you want to logout?";
+      bool? doLogout = await showDialog(context: context,builder:(_)=> OkCancelDialog(message: message));
+      if (doLogout ?? false) {
+          context.read<AppModel>().logout();
+      }
+    }
+    return Container(
+        color: Colors.white,
+        width:250,
+        child:Stack(
+          children: [
+            Column(
+              children: [
+                SizedBox(height: Insets.extraLarge,)
+              ],
+            )
+          ],
+        )
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class _TabMenu extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    List<Expanded> tabButtons = getMainMenuChildren(context).map((btn) => Expanded(child: btn)).toList();
+    return Column(
+        children: [
+          //Top divider
+          Container(width: double.infinity,height: 1,color: Colors.blue,),
+          //Tab buttons 
+          Row(children: tabButtons,)
+        ],
+    );
+  }
 }
