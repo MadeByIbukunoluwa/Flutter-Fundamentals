491a401aaaef177612aabfa75c7a1965e9fb468a
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-04-02 17:15:39 +0100

    feat:modified understanding_constraints with code

--- understanding_constraints/lib/main.dart
+++ understanding_constraints/lib/main.dart
@@ -1,115 +1,207 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(HomePage());
+
+const red = Colors.red;
+const green = Colors.green;
+const blue = Colors.blue;
+const big = const TextStyle(fontSize: 30);
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => FlutterLayoutArticle(<Example>[
+        Example1(),
+        // Example2(),
+        // Example3(),
+        // Example4(),
+        // Example5(),
+        // Example6(),
+        // Example7(),
+        // Example8(),
+        // Example9(),
+        // Example10(),
+        // Example11(),
+        // Example12(),
+        // Example13(),
+        // Example14(),
+        // Example15(),
+        // Example16(),
+        // Example17(),
+        // Example18(),
+        // Example19(),
+        // Example20(),
+        // Example21(),
+        // Example22(),
+        // Example23(),
+        // Example24(),
+        // Example25(),
+        // Example26(),
+        // Example27(),
+        // Example28(),
+        // Example29(),
+      ]);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+abstract class Example extends StatelessWidget {
+  const Example({super.key});
 
-  // This widget is the root of your application.
+  String get code;
+
+  String get explanation;
+}
+
+class FlutterLayoutArticle extends StatefulWidget {
+  final List<Example> examples;
+
+  FlutterLayoutArticle(this.examples);
+
+  @override
+  _FlutterLayoutArticleState createState() => _FlutterLayoutArticleState();
+}
+
+class _FlutterLayoutArticleState extends State<FlutterLayoutArticle> {
+  late int count;
+  late Widget example;
+  late String code;
+  late String explanation;
+
+  @override
+  void initState() {
+    count = 1;
+    code = Example1().code;
+    explanation = Example1().explanation;
+
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(FlutterLayoutArticle oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    var example = widget.examples[count - 1];
+    code = example.code;
+    explanation = example.explanation;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Layout Article',
+        home: SafeArea(
+            child: Material(
+                color: Colors.black,
+                child: FittedBox(
+                    child: Container(
+                  width: 400,
+                  height: 670,
+                  color: Color(0xFFCCCCCC),
+                  child: Column(children: [
+                    Expanded(
+                        child: ConstrainedBox(
+                            constraints: BoxConstraints.tightFor(
+                                width: double.infinity,
+                                height: double.infinity),
+                            child: widget.examples[count - 1])),
+                    Container(
+                        height: 50,
+                        width: double.infinity,
+                        color: Colors.black,
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(mainAxisSize: MainAxisSize.min, children: [
+                            for (int i = 0; i < widget.examples.length; i++)
+                              Container(
+                                  width: 58,
+                                  padding: const EdgeInsets.only(
+                                      left: 4.0, right: 4.0),
+                                  child: button(i + 1))
+                          ]),
+                        )),
+                    Container(
+                        height: 273,
+                        color: Colors.grey[200],
+                        child: Scrollbar(
+                            child: SingleChildScrollView(
+                          key: ValueKey(count),
+                          child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                children: <Widget>[
+                                  Center(child: Text(code)),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  Text(explanation,
+                                      style: TextStyle(
+                                          color: Colors.blue[900],
+                                          fontStyle: FontStyle.italic))
+                                ],
+                              )),
+                        )))
+                  ]),
+                )))));
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Widget button(int exampleNumber) => Button(
+        key: ValueKey("Button$exampleNumber"),
+        isSelected: this.count == exampleNumber,
+        exampleNumber: exampleNumber,
+        onPressed: () {
+          showExample(
+            exampleNumber, 
+              widget.examples[exampleNumber - 1].code,
+              widget.examples[exampleNumber - 1].explanation);
+        },
+      );
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void showExample(int exampleNumber, String code, String explanation) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      this.count = exampleNumber;
+      this.code = code;
+      this.explanation = explanation;
     });
   }
+}
+
+class Button extends StatelessWidget {
+  final Key key;
+  final bool isSelected;
+  final int exampleNumber;
+  final VoidCallback onPressed;
+
+  Button(
+      {required this.key,
+      required this.isSelected,
+      required this.exampleNumber,
+      required this.onPressed})
+      : super(key: key);
+  Widget build(BuildContext context) {
+    return MaterialButton(
+        color: isSelected ? Colors.grey : Colors.grey[800],
+        child: Text(exampleNumber.toString(),
+            style: TextStyle(color: Colors.white)),
+        onPressed: () {
+          Scrollable.ensureVisible(context,
+              duration: Duration(milliseconds: 350),
+              curve: Curves.easeOut,
+              alignment: 0.5);
+          onPressed();
+        });
+  }
+}
+
+class Example1 extends Example {
+  final String code = "Container(color:red)";
+
+  final String explanation = "The screen is the parent of the Container."
+      "It forces the red Container to be exactly the same size of the screen"
+      "\n\n"
+      "So the Container fills the screen and it gets all red.";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Container(
+      color: red,
     );
   }
 }
