e1190d29d51b2ea12c14e7b470f068d98982f405
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-04-05 07:59:21 +0100

    feat:added Case 2 - Parent widget manages state

--- adding_interactivity/lib/main.dart
+++ adding_interactivity/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(title: const Text('Adding_Interactivity')),
           body: Center(
             child: TapBoxA(),
+            // child: FavoriteWidget(),
           ),
         ));
   }
@@ -63,8 +64,7 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
   }
 }
 
-
-// Case 1 : The widget manages its own state 
+// Case 1 : The widget manages its own state
 class TapBoxA extends StatefulWidget {
   const TapBoxA({super.key});
 
@@ -84,21 +84,88 @@ class _TapBoxAState extends State<TapBoxA> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: _handleTap,
-        child: Container(
-          width:200.0,
-          height: 200.0,
-          decoration: BoxDecoration(
-            color:_active ? Colors.lightGreen[700] : Colors.grey[600];
-          ),
-          child: Center(
+      onTap: _handleTap,
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+            color: _active ? Colors.lightGreen[700] : Colors.grey[600]),
+        child: Center(
             child: Text(
-              _active ? 'Active' : 'Inactive',
-              style: const TextStyle(fontSize: 32.0,color:Colors.white),
-            )
-          ),
-        ),
-
+          _active ? 'Active' : 'Inactive',
+          style: const TextStyle(fontSize: 32.0, color: Colors.white),
+        )),
+      ),
     );
   }
 }
+// Case 2
+// Parent widget manages the state and tells its child when to update
+// it can export export its state to its parent through a callback
+
+class TapBoxBContainer extends StatefulWidget {
+  const TapBoxBContainer({super.key});
+
+  @override
+  State<TapBoxBContainer> createState() => _TapBoxBState();
+}
+
+class _TapBoxBState extends State<TapBoxBContainer> { 
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        child: TapBoxB(
+          active:_active,
+          onChanged:_handleTapboxChanged,
+        ),
+    );
+  }
+}
+
+class TapBoxB extends StatelessWidget {
+  const TapBoxB({
+    super.key, 
+    this.active = false, 
+    required this.onChanged
+    });
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override 
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: _handleTap,
+        child: Container(
+          width:200.0,
+          height:200.0,
+          decoration: BoxDecoration(
+            color:active ? Colors.lightGreen[700] :Colors.grey[600]
+          ),
+          child:Center(
+            child:Text(
+                active ? 'Active' : 'Inactive',
+                style:const TextStyle(fontSize: 32.0,color:Colors.white)
+            )
+          )
+        ),
+    );
+  }
+}
+
+
+
+// A mix and match approach 
+
