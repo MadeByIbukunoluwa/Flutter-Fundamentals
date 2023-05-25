aa83f1202e0782ee9f1cefdbb589d0389b078d13
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-05-25 10:40:10 +0100

    feat:fixed errors added comments

--- animations_in_flutter/lib/main.dart
+++ animations_in_flutter/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:animations_in_flutter/tweenanimationbuilder.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/widgets.dart';
+// import 'package:flutter/src/animation/tween.dart';
 import 'dart:math';
 
 // https://medium.com/flutter/custom-implicit-animations-in-flutter-with-tweenanimationbuilder-c76540b47185
@@ -50,7 +51,7 @@ class _TimeMachine extends StatefulWidget {
 }
 
 class _TimeMachineState extends State<_TimeMachine>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   // late final AnimationController _animationController;
   late final AnimationController _repeatingAnimationShort;
   late final AnimationController _repeatingAnimationLong;
@@ -67,7 +68,6 @@ class _TimeMachineState extends State<_TimeMachine>
   @override
   void initState() {
     super.initState();
-
     _repeatingAnimationShort =
         AnimationController(vsync: this, duration: Duration(milliseconds: 3600))
           ..repeat();
@@ -106,6 +106,7 @@ class _TimeMachineState extends State<_TimeMachine>
 
   @override
   Widget build(BuildContext context) {
+      setAnimatables();
     return Stack(children: <Widget>[
       Align(
           alignment: Alignment.center,
@@ -114,16 +115,71 @@ class _TimeMachineState extends State<_TimeMachine>
               turns: _repeatingAnimationLong,
               child: GalaxyFitz())),
       //spinning hourglass
+      Align(
+        alignment: Alignment.center,
+        child: ScaleTransition(
+            scale: _loopingAnimationShort.drive(_scaleCurve),
+            child: RotationTransition(
+                turns: _repeatingAnimationShort,
+                child: Icon(Icons.hourglass_empty,
+                    size: largeIconSize, color: Colors.white))),
+      ),
       //flying home
+      Align(
+        alignment: Alignment.bottomLeft,
+        child: SlideTransition(
+            position: _loopingAnimationShort.drive(_slideCurve),
+            child: RotationTransition(
+                turns: _repeatingAnimationShort,
+                child: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                  size: smallIconSize,
+                ))),
+      ),
       //flying rocket
+      Align(
+        alignment: Alignment.bottomLeft,
+        child: SlideTransition(
+            position: _loopingAnimationShort.drive(_reverseSlide),
+            child: RotationTransition(
+                turns: _repeatingAnimationShort,
+                child: Text("🚀", style: TextStyle(fontSize: smallIconSize)))),
+      ),
       //revolving rocket
+      Align(
+          alignment: Alignment(0.75, 0),
+          child: RotationTransition(
+              turns: _repeatingAnimationLong,
+              alignment: Alignment(-10, 0),
+              child: RotationTransition(
+                  turns: _repeatingAnimationShort,
+                  child:
+                      Text("🚀", style: TextStyle(fontSize: smallIconSize))))),
       // Hidden button - stops time
+      Align(
+        alignment: Alignment.bottomLeft,
+        child: TimeStopper(controllers: [
+          _repeatingAnimationLong,
+          _repeatingAnimationShort,
+          _loopingAnimationShort,
+          _loopingAnimationLong,
+        ]),
+      ),
       //Hidden button flings
+      Align(
+          alignment: Alignment.bottomRight,
+          child: FlingButton(controllers: [_loopingAnimationShort]))
     ]);
   }
 
   void setAnimatables() {
     double deviceHeight = MediaQuery.of(context).size.height;
+  //This error happened because i put the setAnimatables function in the initState method, i fixed this by putting it in the widget build method 
+//  exception has occurred.
+// FlutterError (dependOnInheritedWidgetOfExactType<MediaQuery>() or dependOnInheritedElement() was called before _TimeMachineState.initState() completed.
+// When an inherited widget changes, for example if the value of Theme.of() changes, its dependent widgets are rebuilt. If the dependent widget's reference to the inherited widget is in a constructor or an initState() method, then the rebuilt dependent widget will not reflect the changes in the inherited widget.
+// Typically references to inherited widgets should occur in widget build() methods. Alternatively, initialization based on inherited widgets can be placed in the didChangeDependencies method, which is called after initState and whenever the dependencies change thereafter.)
     double deviceWidth = MediaQuery.of(context).size.width;
 
     _scaleCurve = CurveTween(curve: Curves.easeIn);
@@ -133,7 +189,8 @@ class _TimeMachineState extends State<_TimeMachine>
         end: Offset(
             deviceWidth / smallIconSize, -1 * deviceHeight / smallIconSize));
     _reverseSlide =
-        Tween<Offset>(begin: Offset(deviceWidth / smallIconSize, 2));
+        Tween<Offset>(begin: Offset(deviceWidth / smallIconSize, 2),
+        end: Offset(-2, -1 * deviceHeight / smallIconSize),);
   }
 }
 
