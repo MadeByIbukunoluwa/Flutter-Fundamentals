fe1d645f8e17ab7e04a392daff938f203f545ecb
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-06-10 13:23:23 +0100

    update:added two animations to animatedLogo and uncommented it

--- animations_in_flutter/lib/animationstutorial.dart
+++ animations_in_flutter/lib/animationstutorial.dart
@@ -24,9 +24,10 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     // animation = Tween<double>(begin: 0, end: 300).animate(controller)
     //   ..addListener(() {
     //     setState(() {
+
     //     });
     //   });
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    animation = CurvedAnimation(parent: controller, curve:Curves.easeIn)
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
           controller.reverse();
@@ -38,8 +39,8 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
       ..addStatusListener((status) => print('$status'));
     controller.forward();
   }
-
-  @override
+  // using set state
+  // @override
   // Widget build(BuildContext context) {
   //   return Center(
   //     child: Container(
@@ -50,14 +51,15 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   //   );
   // }
   // AnimatedWidget
-  // Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+  @override
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
 
-  Widget build(BuildContext context) {
-    return GrowTransition(
-      animation: animation,
-      child: const LogoWidget() ,
-    );
-  }
+  // Widget build(BuildContext context) {
+  //   return GrowTransition(
+  //     animation: animation,
+  //     child: const LogoWidget(),
+  //   );
+  // }
 
   @override
   void dispose() {
@@ -72,29 +74,36 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
 
 //Examples of AnimatedWidgets in the Flutter API: AnimatedBuilder, AnimatedModalBarrier, DecoratedBoxTransition, FadeTransition, PositionedTransition, RelativePositionedTransition, RotationTransition, ScaleTransition, SizeTransition, SlideTransition.
 
-// class AnimatedLogo extends AnimatedWidget {
-//   const AnimatedLogo({super.key, required Animation<double> animation})
-//       : super(listenable: animation);
+class AnimatedLogo extends AnimatedWidget {
+  const AnimatedLogo({super.key, required Animation<double> animation})
+      : super(listenable: animation);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final animation = listenable as Animation<double>;
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1.0);
 
-//     return Center(
-//       child: Container(
-//           margin: const EdgeInsets.symmetric(vertical: 10),
-//           height: animation.value,
-//           width: animation.value,
-//           child: const FlutterLogo()),
-//     );
-//   }
-// }
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+
+    return Center(
+        child: Opacity(
+      opacity: _opacityTween.evaluate(animation),
+      child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: const FlutterLogo()
+        ),
+    ));
+  }
+}
 
 // animated builder version
-// An AnimatedBuilder understands how to rende the transition
+// An AnimatedBuilder understands how to render the transition
 // it doesn't know how to render the widget, nor does it manage the Animation objct
 // use animatedBuilder to describe an animation as part of a build method for another widget ,If you simply want to define a widget with a reusable animation, use an AnimatedWidget,
-// examples of animtedBuilders in the Flutter API
+// examples of animatedBuilders in the Flutter API
 // An animatedBuilder is a separate class in the render tree. Like AnimatedWidget, AnimatedBuilder automatically listens to notifications from the Animation Object and marks the widget tree dirty as necessary , so you don't need to call addListener()
 
 class LogoWidget extends StatelessWidget {
@@ -110,8 +119,7 @@ class LogoWidget extends StatelessWidget {
 }
 
 class GrowTransition extends StatelessWidget {
-  const GrowTransition(
-      {required this.child, required this.animation, super.key});
+  const GrowTransition({required this.child, required this.animation, super.key});
 
   final Widget child;
   final Animation<double> animation;
@@ -128,6 +136,7 @@ class GrowTransition extends StatelessWidget {
             child: child,
           );
         },
+        child: child,
       ),
     );
   }
