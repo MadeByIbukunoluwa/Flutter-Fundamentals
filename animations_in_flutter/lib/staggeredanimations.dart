1cd51f8a399d1591758a09d4a94bb63e022a9ab5
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-06-15 02:17:23 +0100

    update:modified animations

--- animations_in_flutter/lib/staggeredanimations.dart
+++ animations_in_flutter/lib/staggeredanimations.dart
@@ -1,10 +1,7 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/widgets.dart';
-
 
 class StaggerDemoApp extends StatelessWidget {
   const StaggerDemoApp({super.key});
@@ -17,115 +14,67 @@ class StaggerDemoApp extends StatelessWidget {
   }
 }
 
-
-
 class StaggerAnimation extends StatelessWidget {
-  const StaggerAnimation({super.key, required this.controller});
+  StaggerAnimation({super.key, required this.controller})
+      : opacity = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+            parent: controller,
+            curve: const Interval(0.0, 0.100, curve: Curves.ease))),
+        width = Tween<double>(begin: 50.0, end: 150.0).animate(CurvedAnimation(
+            parent: controller,
+            curve: const Interval(0.0, 0.100, curve: Curves.ease))),
+        height = Tween<double>(begin: 50.0, end: 150.0).animate(CurvedAnimation(
+            parent: controller,
+            curve: const Interval(0.250, 0.375, curve: Curves.ease))),
+        padding = EdgeInsetsTween(
+                begin: const EdgeInsets.only(bottom: 16),
+                end: const EdgeInsets.only(bottom: 75))
+            .animate(CurvedAnimation(
+                parent: controller,
+                curve: const Interval(0.250, 0.375, curve: Curves.ease))),
+        borderRadius = BorderRadiusTween(
+                begin: BorderRadius.circular(4), end: BorderRadius.circular(75))
+            .animate(CurvedAnimation(
+                parent: controller,
+                curve: const Interval(0.375, 0.500, curve: Curves.ease))),
+        color = ColorTween(begin: Colors.indigo[100], end: Colors.orange[400])
+            .animate(CurvedAnimation(
+                parent: controller,
+                curve: const Interval(0.500, 0.750, curve: Curves.ease)));
 
+  late Animation<double> controller;
+  late Animation<double> opacity;
+  late Animation<double> width;
+  late Animation<double> height;
+  late Animation<EdgeInsets> padding;
+  late Animation<BorderRadius?> borderRadius;
+  late Animation<Color?> color;
 
-  opacity = Tween<double>(
-    begin: 0.0,
-    end: 1.0 
-  ).animate(
-      CurvedAnimation(
-        parent: controller, 
-        curve: const Interval(0.0, 0.100,curve: Curves.ease)
-      )
-  ),
-
-  width = Tween<double>(
-    begin: 50.0,
-    end: 150.0
-  ).animate(
-      CurvedAnimation(
-        parent: controller, 
-        curve: const Interval(
-          0.0, 0.100,
-        curve: Curves.ease)
-      )
-  ),
-
-  height = Tween<double>(
-    begin: 50.0,
-    end: 150.0
-  ).animate(
-      CurvedAnimation(
-        parent: controller, 
-        curve: const Interval(0.250, 0.375,curve: Curves.ease)
-      )
-  ),
-
-  padding = EdgeInsetsTween(
-    begin:const EdgeInsets.only(bottom: 16) ,
-    end:const EdgeInsets.only(bottom: 75) 
-  ).animate(
-      CurvedAnimation(
-        parent: controller, 
-        curve: const Interval(0.250, 0.375,curve: Curves.ease)
-      )
-  ),
-
-  borderRadius = BorderRadiusTween(
-    begin: BorderRadius.circular(4),
-    end:BorderRadius.circular(75)
-  ).animate(
-      CurvedAnimation(
-        parent: controller, 
-        curve: const Interval(0.375, 0.500,curve: Curves.ease)
-      )
-  ),
-
-  color = ColorTween(
-    begin: Colors.indigo[100],
-    end: Colors.orange[400]
-  ).animate(
-      CurvedAnimation(
-        parent: controller, 
-        curve: const Interval(0.500, 0.750,curve: Curves.ease)
-      )
-  );
-
-  
-  final Animation<double> controller;
-  final Animation<double> opacity;
-  final Animation<double> width;
-  final Animation<double> height;
-  final Animation<EdgeInsets> padding;
-  final Animation<BorderRadius?> borderRadius;
-  final Animation<Color?> color;
-
-  Widget _buildAnimation(BuildContext context,Widget? child) {
+  Widget _buildAnimation(BuildContext context, Widget? child) {
     return Container(
-        padding: padding.value,
-        alignment: Alignment.bottomCenter,
-        child: Opacity(
-          opacity: opacity.value,
-            child: Container(
-              width: width.value,
-              height: height.value,
-              decoration: BoxDecoration(
-                color:color.value,
-                border:Border.all(
-                  color: Colors.indigo[300]!,
-                  width:3,
-                ),
-                borderRadius: borderRadius.value
+      padding: padding.value,
+      alignment: Alignment.bottomCenter,
+      child: Opacity(
+        opacity: opacity.value,
+        child: Container(
+          width: width.value,
+          height: height.value,
+          decoration: BoxDecoration(
+              color: color.value,
+              border: Border.all(
+                color: Colors.indigo[300]!,
+                width: 3,
               ),
-            ),
+              borderRadius: borderRadius.value),
         ),
+      ),
     );
-}
-
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller, 
-      builder:_buildAnimation)
+    return AnimatedBuilder(animation: controller, builder: _buildAnimation);
   }
-
 }
-
 
 class StaggerDemo extends StatefulWidget {
   const StaggerDemo({super.key});
@@ -134,35 +83,33 @@ class StaggerDemo extends StatefulWidget {
   State<StaggerDemo> createState() => _StaggerDemoState();
 }
 
-class _StaggerDemoState extends State<StaggerDemo> with SingleTickerProviderStateMixin {
+class _StaggerDemoState extends State<StaggerDemo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
 
-late AnimationController _controller;
+  void initState() {
+    super.initState();
 
-void initState () {
-  super.initState();
-
-  _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 2000))
-}
-
- void dispose() {
-  _controller.dispose();
-  super.dispose();
- }
-
-Future<void> _playAnimation() async {
-  try{
-    await _controller.forward().orCancel;
-    await _controller.reverse().orCancel;
-  } on TickerCanceled {
-
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 2000));
   }
-}
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _playAnimation() async {
+    try {
+      await _controller.forward().orCancel;
+      await _controller.reverse().orCancel;
+    } on TickerCanceled {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Staggered Animation')
-      ),
+      appBar: AppBar(title: Text('Staggered Animation')),
       body: GestureDetector(
         behavior: HitTestBehavior.opaque,
         onTap: () {
@@ -173,12 +120,11 @@ Future<void> _playAnimation() async {
             height: 100,
             width: 300,
             decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.1),
-              border: Border.all(
-                color:Colors.black.withOpacity(0.5)
-              )
+                color: Colors.black.withOpacity(0.1),
+                border: Border.all(color: Colors.black.withOpacity(0.5))),
+            child: StaggerAnimation(
+              controller: _controller.view,
             ),
-            child: StaggerAnimation(controller: _controller.view,),
           ),
         ),
       ),
