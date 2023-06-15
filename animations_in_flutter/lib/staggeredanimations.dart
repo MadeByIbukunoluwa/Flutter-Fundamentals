af225686cd11d3c83856a196fc1bced3fdd19d70
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-06-15 01:52:52 +0100

    update:added animations to staggerAnimation

--- animations_in_flutter/lib/staggeredanimations.dart
+++ animations_in_flutter/lib/staggeredanimations.dart
@@ -5,12 +5,123 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/widgets.dart';
 
-class StaggerAnimation extends StatelessWidget {
-  const StaggerAnimation({super.key, required this.controller}) 
+
+class StaggerDemoApp extends StatelessWidget {
+  const StaggerDemoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ;
+    return const MaterialApp(
+      home: StaggerDemo(),
+    );
+  }
+}
+
+
+
+class StaggerAnimation extends StatelessWidget {
+  const StaggerAnimation({super.key, required this.controller});
+
+
+  opacity = Tween<double>(
+    begin: 0.0,
+    end: 1.0 
+  ).animate(
+      CurvedAnimation(
+        parent: controller, 
+        curve: const Interval(0.0, 0.100,curve: Curves.ease)
+      )
+  ),
+
+  width = Tween<double>(
+    begin: 50.0,
+    end: 150.0
+  ).animate(
+      CurvedAnimation(
+        parent: controller, 
+        curve: const Interval(
+          0.0, 0.100,
+        curve: Curves.ease)
+      )
+  ),
+
+  height = Tween<double>(
+    begin: 50.0,
+    end: 150.0
+  ).animate(
+      CurvedAnimation(
+        parent: controller, 
+        curve: const Interval(0.250, 0.375,curve: Curves.ease)
+      )
+  ),
+
+  padding = EdgeInsetsTween(
+    begin:const EdgeInsets.only(bottom: 16) ,
+    end:const EdgeInsets.only(bottom: 75) 
+  ).animate(
+      CurvedAnimation(
+        parent: controller, 
+        curve: const Interval(0.250, 0.375,curve: Curves.ease)
+      )
+  ),
+
+  borderRadius = BorderRadiusTween(
+    begin: BorderRadius.circular(4),
+    end:BorderRadius.circular(75)
+  ).animate(
+      CurvedAnimation(
+        parent: controller, 
+        curve: const Interval(0.375, 0.500,curve: Curves.ease)
+      )
+  ),
+
+  color = ColorTween(
+    begin: Colors.indigo[100],
+    end: Colors.orange[400]
+  ).animate(
+      CurvedAnimation(
+        parent: controller, 
+        curve: const Interval(0.500, 0.750,curve: Curves.ease)
+      )
+  );
+
+  
+  final Animation<double> controller;
+  final Animation<double> opacity;
+  final Animation<double> width;
+  final Animation<double> height;
+  final Animation<EdgeInsets> padding;
+  final Animation<BorderRadius?> borderRadius;
+  final Animation<Color?> color;
+
+  Widget _buildAnimation(BuildContext context,Widget? child) {
+    return Container(
+        padding: padding.value,
+        alignment: Alignment.bottomCenter,
+        child: Opacity(
+          opacity: opacity.value,
+            child: Container(
+              width: width.value,
+              height: height.value,
+              decoration: BoxDecoration(
+                color:color.value,
+                border:Border.all(
+                  color: Colors.indigo[300]!,
+                  width:3,
+                ),
+                borderRadius: borderRadius.value
+              ),
+            ),
+        ),
+    );
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller, 
+      builder:_buildAnimation)
   }
 
 }
@@ -61,6 +172,12 @@ Future<void> _playAnimation() async {
           child: Container(
             height: 100,
             width: 300,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.1),
+              border: Border.all(
+                color:Colors.black.withOpacity(0.5)
+              )
+            ),
             child: StaggerAnimation(controller: _controller.view,),
           ),
         ),
