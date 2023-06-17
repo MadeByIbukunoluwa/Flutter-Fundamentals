c8311dab1be3f0f442ca2ec23d6e002fea11b39e
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-06-17 03:15:54 +0100

    update:added different slivers and their configurations to showcase their uses

---
+++ advanced_ui/lib/Slivers.dart
@@ -0,0 +1,120 @@
+// https://medium.com/flutter/slivers-demystified-6ff68ab0296f
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SliverApp extends StatelessWidget {
+  const SliverApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('Collapsing List Demo')),
+      body: CollapsingList(),
+    ));
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(
+      {required double this.minHeight,
+      required double this.maxHeight,
+      Widget? this.child});
+
+  final double minHeight;
+  final double maxHeight;
+  final Widget? child;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => max(minHeight, maxHeight);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(
+      child: child,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
+  }
+}
+
+class CollapsingList extends StatelessWidget {
+  const CollapsingList({super.key});
+
+  Widget makeHeader(String headerText) {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: _SliverAppBarDelegate(
+          maxHeight: 200.0,
+          minHeight: 60.0,
+          child: Container(
+            color: Colors.lightBlueAccent,
+            child: Center(child: Text(headerText)),
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color generateRandomColor() {
+      Random random = Random();
+      int r = random.nextInt(256);
+      int g = random.nextInt(256);
+      int b = random.nextInt(256);
+      return Color.fromARGB(255, r, g, b);
+    }
+
+    return CustomScrollView(
+      slivers: <Widget>[
+        makeHeader('Header Section 1 '),
+        SliverGrid.count(
+            crossAxisCount: 3,
+            children: List.generate(
+                9,
+                (i) => Container(
+                      color: generateRandomColor(),
+                      height: 150.0,
+                    ))),
+        makeHeader('Header Section 2'),
+        SliverFixedExtentList(
+          itemExtent: 150.0,
+          delegate: SliverChildListDelegate(List.generate(
+              5,
+              (i) => Container(
+                    color: generateRandomColor(),
+                  ))),
+        ),
+        makeHeader('Header Section 3 '),
+        SliverGrid(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200.0,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+            childAspectRatio: 4.0,
+          ),
+          delegate:
+              SliverChildBuilderDelegate((BuildContext context, index) {
+            return Container(
+                alignment: Alignment.center,
+                color: Colors.teal[100 * (index % 9)],
+                child: Text('grid item $index'));
+          }, childCount: 20),
+        ),
+        makeHeader('Header Section 4'),
+        
+      ],
+    );
+  }
+}
