3d1b626f68445e804edfd59a4ec05a01cd9f67d6
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-06-23 20:56:58 +0100

    feat:added the dailyforecast class  and implemented the required logic

---
+++ advanced_ui/lib/classes/daily_forecast.dart
@@ -0,0 +1,36 @@
+import 'dart:ffi';
+
+class DailyForecast {
+  DailyForecast(
+      {required this.id,
+      required this.imageId,
+      required this.highTemp,
+      required this.lowTemp,
+      required this.description});
+  final int id;
+  final String imageId;
+  final int highTemp;
+  final int lowTemp;
+  final String description;
+
+  getWeekday(int today) {
+    final List<String> _weekdays = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday'
+    ];
+    final int offset = today + id;
+
+    // or int day = offset >= 7 ? offset - 7 : offset 
+    
+    int day = offset % 7;
+
+    return _weekdays[day];
+  }
+
+  getDate(int today) {}
+}
