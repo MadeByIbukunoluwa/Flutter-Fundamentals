ceb94eab4c34e83c71034d306d976c69d090b4da
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-06-25 11:14:41 +0100

    update:renamed _kDummyDatato kDummyData

--- advanced_ui/lib/classes/server.dart
+++ advanced_ui/lib/classes/server.dart
@@ -1,2 +1,13 @@
+import 'package:advanced_ui/SliverWorkshop.dart';
+import 'package:advanced_ui/classes/daily_forecast.dart';
+import "package:advanced_ui/classes/helper.dart";
 
+
+class Server {
+  static List getDailyForecastList() => kDummyData.values.toList();
+  static DailyForecast getDailyForecastByID(int id) {
+    assert(id >= 0 && id <= 6);
+    return kDummyData[id]!; 
+  }
+}
 
